97c8b6c328dd427aa00d63cc9c3e560c7a89e82c
juancarlospaco <juancarlospaco@gmail.com>
2018-09-12 17:17:42 -0300

    Clean Up

--- src/hackpad.nim
+++ src/hackpad.nim
@@ -1,20 +1,15 @@
-import jester, strutils, strformat, os, ospaths, osproc, times, uri, tables, cgi
-import zip/zipfiles
+import jester, strutils, strformat, os, ospaths, osproc, times, uri, tables, cgi, zip/zipfiles
 
 const
-  temp_folder* = getTempDir() / "hackpad"   ## Temporary folder used for temporary files at runtime, etc.
-  strip_cmd*  = "strip --strip-all"         ## Linux Bash command to strip the compiled binary executables.
-  upx_cmd*    = "upx --best --ultra-brute"  ## Linux Bash command to compress the compiled binary executables.
-  sha_cmd*    = "sha1sum --tag"             ## Linux Bash command to checksum the compiled binary executables.
-  html_template = static_read("index.html") ## Main Index HTML Template for the pad.
+  temp_folder* = getTempDir() / "hackpad"       ## Temporary folder used for temporary files at runtime, etc.
+  strip_cmd*  = "strip --verbose --strip-all"   ## Linux Bash command to strip the compiled binary executables.
+  html_template = static_read("index.html")     ## Main Index HTML Template for the pad.
   html_download = static_read("downloads.html") ## HTML Template for Downloads.
-  linux_args* = "" ## Linux Bash command line extra parameters for CrossCompilation on demand, for target Linux.
   windows_args* = "--gcc.exe:/usr/bin/x86_64-w64-mingw32-gcc --gcc.linkerexe:/usr/bin/x86_64-w64-mingw32-gcc"  ## Windows Bash command line extra parameters for CrossCompilation on demand, for target Windows.
 createDir(temp_folder)
-type CrossCompileResult* = tuple[
-  win, winzip, winsha, lin, linzip, linsha, doc, doczip, logs, jsf, jszip, jssha: string]  ## Tuple with full path string to binaries and SHA1 Sum of binaries.
+type CompileResult* = tuple[win, winzip, winsha, lin, linzip, linsha, doc, doczip, logs, jsf, jszip, jssha: string]  ## Tuple with full path string to binaries and SHA1 Sum of binaries.
 
-proc crosscompile*(code, target, opt, release, gc, app, ssls, threads: string): CrossCompileResult =
+proc crosscompile*(code, target, opt, release, gc, app, ssls, threads: string): CompileResult =
   ## Receives code as string and crosscompiles and generates HTML Docs, Strips and ZIPs.
   var win, winzip, winsha, lin, linzip, linsha, doc, doczip, logs, jsf, jszip, jssha: string
   if countLines(code.strip) >= 1:
@@ -29,14 +24,14 @@ proc crosscompile*(code, target, opt, release, gc, app, ssls, threads: string): 
       output: string
       exitCode: int
     # Linux Compilation.
-    (output, exitCode) = execCmdEx(fmt"nim {target} {release} {opt} {gc} {app} {ssls} {threads} {linux_args} --out:{temp_file_bin} {temp_file_nim}")
+    (output, exitCode) = execCmdEx(fmt"nim {target} {release} {opt} {gc} {app} {ssls} {threads} --out:{temp_file_bin} {temp_file_nim}")
     logs &= output
     if exitCode == 0:
       (output, exitCode) = execCmdEx(fmt"{strip_cmd} {temp_file_bin}")
       logs &= output
       if exitCode == 0:
         lin = splitPath(temp_file_bin).tail
-        (output, exitCode) = execCmdEx(fmt"{sha_cmd} {temp_file_bin}")
+        (output, exitCode) = execCmdEx(fmt"sha1sum {temp_file_bin}")
         logs &= output
         if exitCode == 0:
           linsha = output
@@ -53,7 +48,7 @@ proc crosscompile*(code, target, opt, release, gc, app, ssls, threads: string): 
       logs &= output
       if exitCode == 0:
         win = splitPath(temp_file_exe).tail
-        (output, exitCode) = execCmdEx(fmt"{sha_cmd} {temp_file_exe}")
+        (output, exitCode) = execCmdEx(fmt"sha1sum {temp_file_exe}")
         logs &= output
         if exitCode == 0:
           winsha = output
@@ -67,7 +62,7 @@ proc crosscompile*(code, target, opt, release, gc, app, ssls, threads: string): 
     logs &= output
     if exitCode == 0:
       jsf = splitPath(temp_file_js).tail
-      (output, exitCode) = execCmdEx(fmt"{sha_cmd} {temp_file_js}")
+      (output, exitCode) = execCmdEx(fmt"sha1sum {temp_file_js}")
       logs &= output
       if exitCode == 0:
         jssha = output
@@ -86,10 +81,9 @@ proc crosscompile*(code, target, opt, release, gc, app, ssls, threads: string): 
       z.addFile(temp_file_html)
       z.close
       doczip = splitPath(temp_file_html & ".zip").tail
-  let resultaditos: CrossCompileResult = (
-    win: win, winzip: winzip, winsha: winsha.strip, lin: lin, linzip: linzip,
-    linsha: linsha.strip, doc: doc, doczip: doczip, logs: logs, jsf: jsf,
-    jszip: jszip, jssha: jssha)
+  let resultaditos: CompileResult = (
+    win: win, winzip: winzip, winsha: winsha.strip, lin: lin, linzip: linzip, linsha: linsha.strip,
+    doc: doc, doczip: doczip, logs: logs, jsf: jsf, jszip: jszip, jssha: jssha)
   result = resultaditos
 
 proc parseResponseBody(body: string): Table[string, string] =
@@ -133,5 +127,4 @@ routes:
     </div> <details open > <summary>Log</summary>
       <textarea id="log" title="CrossCompilation Logs (Read-Only)" readonly >
       {request.headers.table} {x.logs} </textarea>
-    </details><br><button title="Go Back" onclick="history.back()">Back</button></body>
-    """  # TODO: Add Android support, install https://aur.archlinux.org/packages/android-sdk-ndk-symlink/.
+    </details><br><button title="Go Back" onclick="history.back()">Back</button></body>"""
